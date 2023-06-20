f4f2474e7f229e523f8dbccaa68c311449b8d1c6
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-06-20 13:47:17 -0400

    add undo button

--- commands/panel.js
+++ commands/panel.js
@@ -9,13 +9,13 @@ const u = `<:unchecked:1120528038929637406>`;
 const i = `:exclamation:`;
 
 const buildStatus = () => {
-    let status = ``;
-    Object.entries(json).forEach(game => {
-        if (game[1] === 0) status += `${c} ${game[0]}\n`
-        else if (game[1] === 1) status += `${u} ${game[0]}\n`
-        else if (game[1] === 2) status += `${i} ${game[0]}\n`
-    });
-    return status;
+  let status = ``;
+  Object.entries(json).forEach(game => {
+    if (game[1] === 0) status += `${c} ${game[0]}\n`
+    else if (game[1] === 1) status += `${u} ${game[0]}\n`
+    else if (game[1] === 2) status += `${i} ${game[0]}\n`
+  });
+  return status;
 };
 
 module.exports = {
@@ -29,46 +29,52 @@ module.exports = {
         '1049495201682571334',
         '1053101084190703766',
         '1055625743448682557' // #spam channel in my server
-    ].indexOf(interaction.channel.id) < 0) return interaction.reply({
+      ].indexOf(interaction.channel.id) < 0) return interaction.reply({
       ephemeral: true,
-      embeds: [ new discord.EmbedBuilder().setDescription(`**This command is not for this channel.**`) ]
+      embeds: [new discord.EmbedBuilder().setDescription(`**This command is not for this channel.**`)]
     });
 
     const row1 = new ActionRowBuilder()
       .addComponents(
         new ButtonBuilder()
-          .setLabel('Report the Trending Game!')
-          .setStyle(ButtonStyle.Success)
-          .setCustomId('reporttrending')
+        .setLabel('Report the Trending Game!')
+        .setStyle(ButtonStyle.Success)
+        .setCustomId('reporttrending')
       )
       .addComponents(
         new ButtonBuilder()
-          .setLabel('Report a not Trending Game')
-          .setStyle(ButtonStyle.Danger)
-          .setCustomId('reportnottrending')
+        .setLabel('Report a not Trending Game')
+        .setStyle(ButtonStyle.Danger)
+        .setCustomId('reportnottrending')
       )
     const row2 = new ActionRowBuilder()
       .addComponents(
         new ButtonBuilder()
-          .setLabel('Reset')
-          .setStyle(ButtonStyle.Secondary)
-          .setCustomId('reset')
+        .setLabel('Undo Checking')
+        .setStyle(ButtonStyle.Secondary)
+        .setCustomId('undo')
       )
-    
+      .addComponents(
+        new ButtonBuilder()
+        .setLabel('Reset')
+        .setStyle(ButtonStyle.Secondary)
+        .setCustomId('reset')
+      )
+
     global.thing = interaction;
-    
+
     interaction.reply({
-        embeds: [
-            new EmbedBuilder()
-              .setTitle('Trending Game Manager')
-              .setDescription(`Use this embed to control the daily trending game.\n\n${buildStatus()}`)
-              .setFooter({
-                text: 'thonk#0001 is cool :) | run /key for info'
-              })
-        ],
-        components: [
-            row1, row2
-        ]
+      embeds: [
+        new EmbedBuilder()
+        .setTitle('Trending Game Manager')
+        .setDescription(`Use this embed to control the daily trending game.\n\n${buildStatus()}`)
+        .setFooter({
+          text: 'thonk#0001 is cool :) | run /key for info'
+        })
+      ],
+      components: [
+        row1, row2
+      ]
     })
   }
 }
