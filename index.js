76a2386fb8e0bd9a9ea87c57cd04adb39a7907f8
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-06-24 12:39:47 -0400

    add express

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ const system = require('./system.json')
 const client = new Discord.Client({ intents: [
 	Discord.GatewayIntentBits.Guilds,
 	Discord.GatewayIntentBits.GuildMessages, 
-	Discord.GatewayIntentBits.GuildMessageReactions,
 	Discord.GatewayIntentBits.MessageContent, 
 	Discord.GatewayIntentBits.GuildMembers
 ]})
 
-// Command Handler
 client.commands = new Discord.Collection()
 
 for (const file of fs.readdirSync('./commands').filter(file => file.endsWith('.js'))) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.deploy.name, command);
-}
+};
 
 for (const file of fs.readdirSync('./events').filter(file => file.endsWith('.js'))) {
   const event = require(`./events/${file}`);
   client.on(file.split('.')[0], (t) => event(client, t));
-}
+};
 
 client.login(system.token);
+
+require('express')()
+	.get('/', (req, res) => res.status(200).send('Bot alive!'));
+	.listen(8080);
