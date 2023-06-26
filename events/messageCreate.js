f4bb6ea345c2f4132e3ff1c3605f65e2ef7f2c11
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-06-25 20:15:16 -0400

    change color in >format

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,4 @@
 module.exports = async (client, message, db) => {
     if (message.content.startsWith('>eval ') && message.author.id === '1003477997728313405') eval(message.content.split('>eval ')[1]);
-    else if (message.content.startsWith('>format')) message.reply('**EXAMPLE TREND EMBED USAGE:**\n```\n.embed <#926282726053650464> d25251 Trending Game: INSERT GAME HERE\n```\n*do not forget to credit the person & ping*');
+    else if (message.content.startsWith('>format')) message.reply('**EXAMPLE TREND EMBED USAGE:**\n```\n.embed <#926282726053650464> ffffff Trending Game: INSERT GAME HERE\n```\n*do not forget to credit the person & ping*');
 };
